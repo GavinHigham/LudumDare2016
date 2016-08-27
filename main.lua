5a7d93b400a4e38cadce25976e2c14f13e2e9e17
enricotj <taiphoon007@gmail.com>
2016-08-26 20:34:14 -0700

    Basic LUA Functions, Basic Asset Folders

---
+++ main.lua
@@ -0,0 +1,24 @@
+--[[
+
+	Ancient Technology Ludumn Dare Game Jam 2016
+	By: Gavin Higham, Josh Maurer, and Tai Enrico
+
+]]
+
+enemies = {}
+
+function love.load()
+
+end
+
+function love.update(dt)
+	
+end
+
+function love.mousepressed(x, y, button)
+	
+end
+
+function love.draw()
+	
+end
